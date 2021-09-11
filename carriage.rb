25dd3b1218e9b6c39508afc8e3e7a8a5b5de8cd8
imepanov <87714341+imepanov@users.noreply.github.com>
2021-09-12 00:05:26 +0500

    Add files via upload

---
+++ carriage.rb
@@ -0,0 +1,8 @@
+class Carriage
+  attr_reader :type, :number
+
+  def initialize(number, type)
+    @number = number
+    @type = type
+  end
+end
---
+++ station.rb
@@ -0,0 +1,16 @@
+class Station
+attr_reader :type, :trains, :name, :stations, :start, :finish, :current_index, :next_station, :previous_station
+  def initialize(name)
+    @name = name
+    @trains = []
+  end
+
+  def arrived(train)
+    @trains << train
+  end
+
+  def left(train)
+    @trains.delete(train)
+  end
+
+end
